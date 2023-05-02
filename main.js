92dd1c5a760ba6549a7b15909037c742aa67055e
Emerson Rabelo <68362954+EmersonRabelo@users.noreply.github.com>
2023-05-01 23:36:39 -0300

    Change: New features

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const cam = document.querySelector('#video')
+const btnProx = document.querySelector('#btn-prox')
 
 Promise.all([ // Retorna apenas uma promisse quando todas já estiverem resolvidas
 
@@ -30,11 +31,20 @@ cam.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video) // Criando canvas para mostrar nossos resultador
     document.body.append(canvas) // Adicionando canvas ao body
 
-    const displaySize = { width: cam.width, height: cam.height } // criando tamanho do display a partir das dimenssões da nossa cam
+    const displaySize = { width: 720, height: 480 } // criando tamanho do display a partir das dimenssões da nossa cam
 
     faceapi.matchDimensions(canvas, displaySize) // Igualando as dimensões do canvas com da nossa cam
 
-    setInterval(async () => { // Intervalo para detectar os rostos a cada 100ms
+    let tics = setInterval(detectionAPI, 1000);
+
+    btnProx.addEventListener('click', e => { 
+        tics = setInterval(detectionAPI, 1000);
+    })
+
+    async function detectionAPI() {
+
+        btnProx.setAttribute('hidden', 'hidden')
+
         const detections = await faceapi.detectAllFaces(
             cam, // Primeiro parametro é nossa camera
             new faceapi.TinyFaceDetectorOptions() // Qual tipo de biblioteca vamos usar para detectar os rostos
@@ -50,8 +60,42 @@ cam.addEventListener('play', () => {
         canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height) // Apagando nosso canvas antes de desenhar outro
 
         faceapi.draw.drawDetections(canvas, resizedDetections) // Desenhando decções
-        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections) // Desenhando os pontos de referencia
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections) // Desenhando expressões
+        //faceapi.draw.drawFaceLandmarks(canvas, resizedDetections) // Desenhando os pontos de referencia
+        //faceapi.draw.drawFaceExpressions(canvas, resizedDetections) // Desenhando expressões
 
-    }, 100);
+        console.log(detections[0].detection._score)
+
+        const __score = detections[0].detection._score
+
+        if (__score >= 0.90) {
+            console.log('é um rosto humano!')
+            stopExec(tics)
+            makeAccounts()
+
+            btnProx.removeAttribute('hidden')
+        }
+
+    }
+
+
+
+    
+
+    function stopExec(interval) {
+        clearInterval(interval)
+    }
+
+    function makeAccounts(){
+        let accounts = localStorage.getItem('amount')
+
+        accounts = parseInt(accounts)
+
+        if (accounts){
+            accounts += 1
+            localStorage.setItem('amount', accounts)
+        } else {
+            localStorage.setItem('amount', 1)
+        }
+    }
+
 })
